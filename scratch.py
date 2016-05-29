fbc5d61b5bbdca38510cb153d12539128bc253b9
Ethan <ethanabrooks@gmail.com>
2016-05-28 18:48:16 -0700

    scratch.py

--- scratch.py
+++ scratch.py
@@ -2,9 +2,7 @@ import theano
 import theano.tensor as T
 import numpy as np
 
-x = T.constant(np.arange(9).reshape(3, 3))
-i = T.constant(np.arange(3))
-z = x[:, i]
-print(theano.function([], z)())
+x = T.constant(np.ones((1024, 40000)))
+print(theano.function([], x)())
 
 
