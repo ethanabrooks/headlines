15b164c04aa4eac1f5c1d4ea7267169605c569d0
Ethan <ethanabrooks@gmail.com>
2016-05-29 11:13:29 -0700

    fixed div by zero error in print_progress

--- main.py
+++ main.py
@@ -183,6 +183,8 @@ def running_average(loss, new_loss, instances_processed, num_instances):
 
 
 def format_time(seconds):
+    if seconds is None:
+        return float("nan")
     minutes, seconds = divmod(seconds, 60)
     hours, minutes = divmod(minutes, 60)
     return ":".join((str(int(t)) for t in (hours, minutes, seconds)))
@@ -191,9 +193,7 @@ def format_time(seconds):
 def print_progress(epoch, instances_processed, num_instances, loss, start_time):
     progress = round(float(instances_processed) / num_instances, ndigits=3)
     elapsed_time = time.time() - start_time
-    eta = elapsed_time / progress
-    print(eta)
-    print(format_time(eta))
+    eta = elapsed_time / progress if progress else None
     elapsed_time, eta = map(format_time, (elapsed_time, eta))
     print('\r###\t{:<10d}{:<10.1%}{:<10.5f}{:<10}{:<10}###'
           .format(epoch, progress, float(loss), elapsed_time, eta), end='')
