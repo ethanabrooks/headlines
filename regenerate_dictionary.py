e4ae4d73bde419c77cdaa47f38707cb34ea51096
Ethan <ethanabrooks@gmail.com>
2016-05-29 10:26:08 -0700

    fixed up regenerate_dictionary

--- regenerate_dictionary.py
+++ regenerate_dictionary.py
@@ -10,19 +10,16 @@ special_words = [['<pad>'], ['<go>'], ['<oov>']]
 s = parser.parse_args()
 dictionary = dict()
 reverse_dictionary = dict()
-dict_filename = 'dict.txt'
-dict_path = os.path.join(s.data_dir, dict_filename)
 for set_name in ['test', 'train']:
-    data_filename = 'dict.txt'
-    data_path = os.path.join(s.data_dir, dict_filename)
-    with open(dict_path) as handle:
+    word_count_filename = 'train.' + set_name + '.dict'
+    word_count_path = os.path.join(s.data_dir, word_count_filename)
+    with open(word_count_path) as handle:
         for line in handle:
             word, idx = line.split()
             dictionary[float(idx)].append(word)
             reverse_dictionary[word] = idx
 
-    dict_filename = 'train.' + set_name + '.dict'
-    with open(dict_filename, 'w+') as handle:
+    with open('dict.txt', 'w+') as handle:
         for i, word_list in enumerate(special_words + dictionary.values()):
             for word in word_list:
                 handle.write('{} {}\n'.format(word, i))
