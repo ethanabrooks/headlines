6ec9689169a0c177c7e227b92265d05a9bb832a3
Ethan <ethanabrooks@gmail.com>
2016-05-28 18:32:26 -0700

    rewrote regenerate_dictionary

--- regenerate_dictionary.py
+++ regenerate_dictionary.py
@@ -1,29 +1,32 @@
 import argparse
 import os
-from collections import defaultdict
+import pickle
+from operator import itemgetter
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--data_dir', type=str, default='/data2/jsedoc/fb_headline_first_sent/',
                     help='path to data')
 
+n = 40 * 1000
 special_words = [['<pad>'], ['<go>'], ['<oov>']]
 s = parser.parse_args()
+counts = []
 for set_name in ["article", "title"]:
-    dictionary = dict()
-    reverse_dictionary = dict()
+    dictionary, reverse_dictionary = dict(), dict()
     dict_filename = 'train.' + set_name + '.dict.orig'
     print(dict_filename)
     dict_path = os.path.join(s.data_dir, dict_filename)
     print(dict_path)
     with open(dict_path) as handle:
         for line in handle:
-            word, idx = line.split()
-            dictionary[float(idx)].append(word)
-            reverse_dictionary[word] = idx
+            word, count = line.split()
+            counts.append((word, float(count)))
 
-    dict_filename = 'train.' + set_name + '.dict'
-    with open(dict_filename, 'w+') as handle:
-        for i, word_list in enumerate(special_words + dictionary.values()):
-            for word in word_list:
-                handle.write('{} {}\n'.format(word, i))
-print(i)
+
+top_n_counts = sorted(counts, key=itemgetter(1), reverse=True)[:n]
+for word, _ in top_n_counts:
+    dictionary[word] = len(dictionary)
+
+dict_filename = 'dict.pkl'
+with open(dict_filename, 'w+') as handle:
+    pickle.dump(dictionary, handle)
