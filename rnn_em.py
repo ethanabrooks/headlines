77fd90ef2512e2aa380334853a88195881bef300
Ethan <ethanabrooks@gmail.com>
2016-06-08 09:03:31 -0700

    asd

--- rnn_em.py
+++ rnn_em.py
@@ -170,7 +170,7 @@ class Model(object):
             # MODEL INPUT AND OUTPUT
             # eqn 9
             # self.Wx = Print('Wx', ['mean'])(self.Wx)
-            # self.Wh = Print('Wh', ['mean'])(self.Wh)
+            self.Wh = Print('Wh', ['mean'])(self.Wh)
             h = T.dot(c, self.Wh) + T.dot(x_i, self.Wx) + self.bh  # [instances, hidden_size]
             h = Print('h', ['mean'])(h)
 
