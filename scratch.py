8050c6deed3e75fd9bd377b20a6b2b8ee86958bc
Ethan <ethanabrooks@gmail.com>
2016-06-22 09:50:05 -0700

    got things working with a separate Lambda layer per output

--- scratch.py
+++ scratch.py
@@ -1,33 +1,35 @@
 from __future__ import print_function
 
-import datetime
-import theano
-import theano.tensor as T
+from keras.layers import Dense, Input, Lambda
+from keras.models import Sequential, Model
 import numpy as np
-import time
-import sys
-
-from keras.layers import Dense
-from keras.models import Sequential
-
-from main import unpickle
 
 batch_size = 2
-input_dim = 3
+input_dim = 1
 midway_dim = 4
-output_dim = 5
+output_dim = 1
 train_y_dim = 6
-X_batch = np.arange(
-    batch_size * input_dim).reshape(
-    batch_size, input_dim)
+timesteps_i = 5
+timesteps_o = 6
+
+X_batch = np.ones((
+    batch_size, input_dim))
+# .reshape( batch_size, 1, input_dim)
 Y_batch = np.arange(
     batch_size * midway_dim).reshape(
-    batch_size, midway_dim)
+    batch_size, 1, midway_dim)
 
-encoder = Sequential()
-encoder_layer = Dense(midway_dim, input_dim=input_dim)
-encoder.add(encoder_layer)
+dense = Dense(output_dim, input_shape=(input_dim,))
+model1 = Sequential([
+    dense
+])
 
-encoder.compile(loss='categorical_crossentropy',
-                optimizer='sgd')
-encoder.train_on_batch(X_batch, Y_batch)
+x = Input(shape=(input_dim,))
+f = lambda input: input + 1
+# layers = []
+# for output in f(x):
+#     layers.append(Lambda(lambda _: output)(x))
+output = Lambda(lambda _: f(x))(x)
+model = Model(input=x, output=output)
+
+print(model.predict(X_batch))
